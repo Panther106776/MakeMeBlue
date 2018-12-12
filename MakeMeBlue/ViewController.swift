4d98f5bd7b97ea7c4fdaffb32b900b63ce1885f9
GERARDO FLORES <rl106776@rlas-macair-15.local>
2018-12-12 10:42:25 -0600

    Initial Commit

---
+++ MakeMeBlue/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  MakeMeBlue
+//
+//  Created by GERARDO FLORES on 8/31/18.
+//  Copyright © 2018 GERARDO FLORES. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var Makemebluelabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       Makemebluelabel.textColor = .blue
+    }
+
+   
+}
+
